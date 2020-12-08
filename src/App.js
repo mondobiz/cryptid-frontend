79081e10e49ffadbab88022e58a14fa4306143f1
mondobizz <caseyjhatley@yahoo.com>
2020-12-08 17:58:01 -0500

    got signup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
-import './App.css';
-import DashBoardPage from './pages/DashBoardPage'
 import HomePage from './pages/HomePage'
+import DashBoardPage from './pages/DashBoardPage'
 import LoginPage from './pages/LoginPage'
 import SignupPage from './pages/SignupPage'
 
+import { Switch, Route } from 'react-router-dom';
 
+import './App.css';
 
+// TODO import switch and route
 function App() {
   return (
     <div className="App">
       <Header />
+      <Switch>
+        <Route exact path="/" render={ props => 
+          <HomePage />  
+        } />
+        
+        <Route exact path="/dashboard" render={ props => 
+          <DashBoardPage />  
+        } />
+        
+        <Route exact path="/signup" render={ props => 
+          <SignupPage />  
+        } />
+        
+        <Route exact path="/login" render={ props => 
+          <LoginPage />  
+        } />
+      </Switch>
       <Footer />
     </div>
   );
---
+++ src/services/tokenService.js
@@ -0,0 +1,22 @@
+function setToken(token) {
+    if(token) {
+        localStorage.setItem('token', token);
+    } else {
+        localStorage.removeItem('token');
+    }
+}
+function getUserFromToken() {
+
+}
+function getToken() {
+
+}
+function removeToken() {
+    localStorage.removeItem('token');
+}
+export {
+    setToken,
+    getUserFromToken,
+    getToken,
+    removeToken
+}
