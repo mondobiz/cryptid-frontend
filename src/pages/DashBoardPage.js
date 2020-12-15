85916323285e3ca7327e13680f757c9f11041c8a
mondobizz <caseyjhatley@yahoo.com>
2020-12-15 02:31:13 -0500

    have mongodb connected to the frontend

--- src/pages/DashBoardPage.js
+++ src/pages/DashBoardPage.js
@@ -1,9 +1,22 @@
+// import { useState } from 'react';
+
+import Creatures from '../components/creatures/Creatures';
+
 export default function DashBoardPage(props) {
+    // const [ formState, setFormState ] = useState({
+    //     image: "",
+    //     name: "",
+    //     description: "",
+    //     lastKnownLocation: ""
+    // });
+
+
     return (
         <main className="Page">
-            <h1>
-                DashBoardPage
-            </h1>
+            <h1>Cryptid Creatures!</h1>
+            <Creatures />
+
+    
         </main>
     );
 }
