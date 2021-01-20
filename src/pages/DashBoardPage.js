787d3b5f66080238ecb15a3560fbcec9994b45bc
mondobizz <caseyjhatley@yahoo.com>
2021-01-20 01:11:34 -0500

    got create and delete working

--- src/pages/DashBoardPage.js
+++ src/pages/DashBoardPage.js
@@ -1,23 +1,23 @@
 // import { useState } from 'react';
 import './pages.css'
-
+import { useState, useEffect } from 'react';
 import Creatures from '../components/creatures/Creatures';
 
 export default function DashBoardPage(props) {
-    // const [ formState, setFormState ] = useState({
-    //     image: "",
-    //     name: "",
-    //     description: "",
-    //     lastKnownLocation: ""
-    // });
 
+    const [createFormVisible, setCreateFormVisible] = useState(false);
+
+    function toggleForm() {
+        setCreateFormVisible(!createFormVisible);
+    }
 
     return (
         <main className="Page">
             <h1>Cryptid Creatures of the Week!</h1>
-            <Creatures />
+            <button onClick={toggleForm}>Add new cryptid</button>
+            <Creatures createFormVisible={createFormVisible}/>
 
-    
+
         </main>
     );
 }
